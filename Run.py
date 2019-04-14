8ce5b1cd2e4010b6d52324f3f449d81bf15155ab
Kumar Manasvi <kumarmanasvi@v1040-wn-rt-c-71-143.campus-dynamic.uwaterloo.ca>
2019-04-14 18:15:42 -0400

    confirming file addition

--- Run.py
+++ Run.py
@@ -2,13 +2,14 @@ import ssl
 import urllib.request
 import nltk
 import fasttext
-from html.parser import HTMLParser 
+from html.parser import HTMLParser
+from nltk.corpus import stopwords
 
-#https://stackoverflow.com/questions/328356/extracting-text-from-html-file-using-python
 
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
+
 
 import ssl
 
@@ -44,7 +45,6 @@ def strip_tags(html):
     s.feed(html)
     return s.get_data()
   
-
 for urls in search(query, tld='com', lang='en', num=1, start=0, stop=1, pause=2.0):
     response=urllib.request.urlopen(urls, context=ctx)
     html = response.read()
@@ -60,11 +60,34 @@ for urls in search(query, tld='com', lang='en', num=1, start=0, stop=1, pause=2.
     lines = (line.strip() for line in text.splitlines())
     chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
     text = '\n'.join(chunk+" __label__" for chunk in chunks if chunk)
-    f = open('data.txt','w+')
+    f = open('data.txt','w')
     f.write(str(text.encode('utf8')))
+    f.close()
 
+f1 = open('sampleMessage.txt','r')
+user_input_message = f1.read()
+f1.close()
+s=set(stopwords.words('english'))
+list(filter(lambda w: not w in s,user_input_message.split()))
+lines = (line.strip() for line in user_input_message.splitlines())
+chunks = (phrase.strip() for line in lines for phrase in user_input_message.split("  "))
+user_input_message = '\n'.join(chunk+" __label__" for chunk in chunks if chunk)
+f2 = open('test.txt','w')
+f2.write(str(user_input_message.encode('utf8')))
+f2.close()
 classifier = fasttext.supervised('data.txt', 'model',label_prefix='__label__')
-result = classifier.test('data.txt')
+result = classifier.test('test.txt')
 print ('P@1:', result.precision)
 print ('R@1:', result.recall)
 print ('Number of examples:', result.nexamples)
+texts = [user_input_message]
+labels = classifier.predict(texts)
+print (labels) 
+
+
+
+
+
+
+
+
